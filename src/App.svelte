631086ff82ec691cc67dfe3d4f9a6b69c73c90b8
Brett Slaski <brett@thatconference.com>
2021-02-07 20:38:14 -0600

    new vars added

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	export let name;
 	const mssg = __testApp.env.BRETT_S;
+	const nodeenv = __testApp.env.NODE_ENV;
 	
 </script>
 
@@ -8,6 +9,7 @@
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<p>This is our env message: <strong>{mssg}</strong></p>
+	<p>NODE_ENV: {nodeenv}</p>
 </main>
 
 <style>
